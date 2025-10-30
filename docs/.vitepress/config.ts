a7943c1b346e3d34ef4a4d43ad80a0eb9ff4ff1c
Reiddos <1289425578@qq.com>
2025-10-30 19:53:40 +0800

    Initial commit: Add blog files

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,169 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+//  GitHub 仓库名
+  base: '/Yuki/', // <-- 这是为 GitHub Pages 准备的
+  
+  lastUpdated: true,
+  title: "转生成为AI小子",
+  description: "per aspra ad astra",
+  lang: 'zh-CN',                         // 设置语言为中文
+  head: [
+      [ 'link', { rel: 'icon', href: '/Yuki/01.ico' } ]
+  ],
+
+  themeConfig: {
+    //页脚
+    footer: {
+      message: 'per aspera ad astra',
+    },
+    //搜索
+    search: {
+      provider: 'local'
+    },
+
+
+    editLink: {
+      pattern: 'https://github.com/Reiddos/Yuki/edit/main/docs/:path',
+    },
+
+
+
+    nav: [
+      //首页
+      { text: '首页', link: '/' },
+      //修考
+      { 
+        text: '修考',
+        items:[
+          {
+            text: '数学',
+            items:[
+              { text:'微积分',link:'/math/calculus/main'},
+              { text: '线性代数', link: '/math/algebra/linear algebra' }
+            ]
+          }
+        ]
+      },
+      
+
+      //cs
+      { 
+        text: 'CS',
+        items:[
+          {
+            items:[
+              { text:'导论',link:'/cs/cs_intro/intro'},
+              { text: 'CS61编程', link: '/cs/cs61/intro' }
+            ]
+          }
+        ]
+      },
+
+    ],
+    
+
+
+
+    
+    // 侧边栏
+    sidebar: {
+      // 当用户在 /math/ 目录下时，显示这个侧边栏
+      '/math/calculus/': [
+        {
+          text: '高等数学', // 章节标题
+          link:'/math/calculus/main',
+          items: [
+            {
+              text: '第一部分',
+              link:'/math/calculus/part1',
+              collapsed: true,
+              items:[            
+                { text: '第一章：函数', link: '/math/calculus/01' },
+                { text: '第二章：极限', link: '/math/calculus/02' },
+              ]
+            }
+
+          ]
+        }
+      ],
+
+      
+      //线代
+      '/math/algebra/': [
+        {
+          text: '线性代数',
+          link:'/math/algebra/linear algebra',
+          items: [
+            {
+              text: '第一部分',
+              link:'/math/algebra/part1',
+              collapsed: true,
+              items:[
+                { text: '第一章：矩阵',
+                  link: '/math/algebra/01',
+
+                },
+                { text: '第二章：行列式', link: '/math/algebra/02' }
+              ]
+            }
+          ]
+        }
+      ],
+      
+
+      '/cs/cs_intro/': [
+        {
+          text: 'CS导论',
+          link:'/cs/cs_intro/intro',
+          items: [
+            {
+              text: '第一部分',
+              //link:'/cs/cs_intro/intro',
+              collapsed: true,
+              items:[
+                { text: '第一章',
+                  //link: '/math/algebra/01',
+
+                },
+                //{ text: '第二章', link: '/math/algebra/02' }
+              ]
+            }
+          ]
+        }
+      ],
+
+      '/cs/cs61/': [
+        {
+          text: 'CS61',
+          link:'/cs/cs61/intro',
+          items: [
+            {
+              text: '第一部分',
+              //link:'/cs/cs_intro/intro',
+              collapsed: true,
+              items:[
+                { text: '第一章',
+                  //link: '/math/algebra/01',
+
+                },
+                //{ text: '第二章', link: '/math/algebra/02' }
+              ]
+            }
+          ]
+        }
+      ],
+
+
+
+    }
+
+
+
+
+  }
+
+
+
+})
---
+++ docs/index.md
@@ -0,0 +1,52 @@
+---
+# ---------------------------------
+# 1. 启用 Home 布局
+# ---------------------------------
+layout: home
+
+# ---------------------------------
+# 2. "Hero" 区域配置
+# (页面顶部的大标题、Slogan 和按钮)
+# ---------------------------------
+hero:
+  # 主标题
+  name: "转生成为AI小子"
+  # 副标题
+  text: "学习笔记"
+  # Slogan 或描述
+  tagline: "強くなりたい "
+  # 行动按钮
+  actions:
+    - theme: brand
+      text: 开始学习 →
+      link: /math/calculus/main  # 链接到你的第一篇高数笔记
+    # - theme: alt
+    #   text: 线性代数
+    #   link: /main/algebra/linear algebra # 链接到你的线代笔记
+
+# ---------------------------------
+# 3. "Features" 区域配置
+# (Slogan 下方的多栏特性介绍)
+# ---------------------------------
+features:
+  - icon: 
+        src: /03.ico
+    title: 修考
+    details: 情报理工，eeis复习笔记。
+  - icon: 
+      src: /favicon.ico
+    title: 编程
+    details: 记录CS基础学习笔记。
+  - icon: 
+        src: /02.ico
+    title: AI
+    details: 强化学习，深度学习，机器人学相关内容。
+---
+
+
+## 关于这个网站
+
+这是一个基于 VitePress 搭建的个人复习笔记网站。感谢恩施KJ,GFC。
+
+记录修考gap year的学习过程，督促自己日拱一卒，也希望也能为其他人带来一些帮助。
+
